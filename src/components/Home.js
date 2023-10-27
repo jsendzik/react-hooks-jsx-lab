3628f994343f62b8bc458990039e0dc4f4d402ff
Jacob Sendzik <jake.sendzik@gmail.com>
2023-10-27 12:48:40 -0400

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const myString = `${name} is a Web Developer from ${city}`
+const divStyle = {
+  color: "firebrick"
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id="home">
+    <h1 style={divStyle}>{ myString }</h1>
+  </div>)
 }
 
 export default Home;
